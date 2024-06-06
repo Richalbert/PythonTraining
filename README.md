4a1ec6300e3748017a0e24dc8f2ee43dad2d5e10
Richalbert <richalbert@free.fr>
2024-06-06 11:38:43 +0200

    premier essai avec tkinter

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Repository de test pour Git, GitHub et VSCode
 
 Le wiki contient des aides
+
+* quelques tests avec la creation d'interface graphique avec Tkinter
--- essai.py
+++ essai.py
@@ -1,8 +1,34 @@
-print("Hello World")
+# print("Hello World")
 
-import sys
-print(sys.version)
+# import sys
+# print(sys.version)
 
-print("Premiere version a sauver dans mon GitHub")
+# print("Premiere version a sauver dans mon GitHub")
 
-print("Seconde version")
+# print("Seconde version")
+
+
+import tkinter
+
+# Creation de la fenetre principale
+fenetre = tkinter.Tk()
+
+# Un titre pour la fenetre
+fenetre.title('Fenetre principale')
+
+# Position de la fenetre "largeurxhauteu+x+y"
+fenetre.geometry("640x480+0+0")
+
+# # Les dimensions de la fenetre
+# largeur = 440
+# hauteur = 280
+# fenetre.minsize(largeur, hauteur)
+# fenetre.maxsize(largeur+200, hauteur+200)
+
+# Pour bloquer le redimensionnement de la fenetre ou pas
+fenetre.resizable(width=False, height=True)
+
+# Rendre la fenetre active
+fenetre.mainloop()
+
+
